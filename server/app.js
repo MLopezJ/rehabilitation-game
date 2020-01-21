b15d0fbe3fd02ce49f9b11a9433f202a1c0c7ec0
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-21 11:29:16 -0600

    refactor the way the cors errors are prevent

--- server/app.js
+++ server/app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //prevent cors errors 
-app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({}); //201
-    }
-
-    next();
-});
+app.use(cors());
 
 module.exports = app;
