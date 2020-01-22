01cfdf0f5acbc3bad4dfd921f7ed6747f2c3c9cc
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-22 16:13:46 -0600

    import socket.io and refactor the server.listen fuction

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const app = require('./app');
 const port = 5000;
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-server.listen(port);
-console.log('* Server is running on port ', port);
+
+server.listen(port, () => {
+	console.log('* Server is running on port ', port);
+});
