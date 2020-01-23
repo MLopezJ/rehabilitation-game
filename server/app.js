1b7a632f394825baf6af30cd63546977fe3b1274
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-23 16:31:59 -0600

    relative paths

--- server/app.js
+++ server/app.js
@@ -3,15 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(express.static('C:/Users/mlopez-as/Documents/rehabilitation-game/client/'));
+app.use(express.static('../client/'));
 
 //prevent cors errors 
 app.use(cors());
 
 app.get('/', function(req, res){
-    res.sendFile('C:/Users/mlopez-as/Documents/rehabilitation-game/client/' + '/index.html');
+    res.sendFile('../client/' + '/index.html');
 });
 
 module.exports = app;
