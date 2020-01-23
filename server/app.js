4f8caf6f57504d9f56549567bc923c2e62c68b95
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-23 16:29:07 -0600

    update game route

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(express.static('C:/Users/mlopez-as/Documents/rehabilitation-game/client/t-rex-runner'));
+app.use(express.static('C:/Users/mlopez-as/Documents/rehabilitation-game/client/'));
 
 //prevent cors errors 
 app.use(cors());
 
 app.get('/', function(req, res){
-    res.sendFile('C:/Users/mlopez-as/Documents/rehabilitation-game/client/t-rex-runner' + '/index.html');
+    res.sendFile('C:/Users/mlopez-as/Documents/rehabilitation-game/client/' + '/index.html');
 });
 
 module.exports = app;
