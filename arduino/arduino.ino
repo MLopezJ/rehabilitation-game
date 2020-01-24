3cc17cd04a9997cc7ea109371c12266065db545f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-24 16:27:13 -0600

    add code for read data from the accelerometer in the Arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,48 +1,82 @@
-const int buttonPin = 2;   // the number of the pushbutton pin
-const int ledPin =  13;   // the number of the LED pin
-//debounce
-int buttonState = 0;          // variable for reading the pin status
-int lastButtonState = LOW;   // the previous reading from the input pin
-int ledState = HIGH;  
+//Analog read pins
+const int xPin = 0;
+const int yPin = 1;
+const int zPin = 2;
 
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+//The minimum and maximum values that came from
+//the accelerometer while standing still
+//You very well may need to change these
+int minVal = 265;
+int maxVal = 402;
+int angle = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(buttonPin, INPUT);    // declare pushbutton as input
-  pinMode(ledPin, OUTPUT);
+//to hold the caculated values
+double x;
+double y;
+double z;
 
-  digitalWrite(ledPin, ledState);
+void setup(){
+Serial.begin(9600);
 }
 
 void loop(){
-  int reading = digitalRead(buttonPin);  // read input value
-  
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
+//read the analog values from the accelerometer
+int xRead = analogRead(xPin);
+int yRead = analogRead(yPin);
+int zRead = analogRead(zPin);
+x = analogRead(xPin);
+y = analogRead(yPin);
+z = analogRead(zPin);
+// show x, y and z-values
+/*
+Serial.print(" x = ");
+Serial.print(x);
+Serial.print("  y = ");
+Serial.print(y);
+Serial.print("  z = ");
+Serial.print(z);*/
+// show angle
+//Serial.print(" X angle = ");
+//Serial.println(constrain(map(y,349,281,0,90),0,90));
+//Serial.println(constrain(map(x,349,281,0,90),0,90));
 
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-        Serial.println("JUMP");
-      }
-    }
-  }
-
-  // set the LED:
-  digitalWrite(ledPin, ledState);
-
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;
+angle = constrain(map(x,349,281,0,90),0,90);
+/*
+Serial.print("Angle ");
+Serial.println(angle);
+*/
+//Serial.println(angle);
+if (angle <= 65 and angle >= 50){
+  Serial.println("JUMP");
 }
+
+delay(100);
+}
+
+/*
+
+//convert read values to degrees -90 to 90 – Needed for atan2
+int xAng = map(xRead, minVal, maxVal, -90, 90);
+int yAng = map(yRead, minVal, maxVal, -90, 90);
+int zAng = map(zRead, minVal, maxVal, -90, 90);
+
+//Caculate 360deg values like so: atan2(-yAng, -zAng)
+//atan2 outputs the value of -π to π (radians)
+//We are then converting the radians to degrees
+x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+
+//Output the caculations
+Serial.print("x: ");
+Serial.print(x);
+Serial.print(" | y: ");
+Serial.print(y);
+Serial.print(" | z: ");
+Serial.println(z);
+
+delay(1000);//just here to slow down the serial output – Easier to read
+
+
+}*/
